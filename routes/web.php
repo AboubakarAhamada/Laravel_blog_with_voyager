0df21c3734806963cd8b0563fce31e6bfc45a52a
AboubakarAhamada <aboumarshal@hotmail.fr>
2020-12-08 16:59:55 +0100

    Controller, Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('posts',[MainController::class, 'index']);
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
